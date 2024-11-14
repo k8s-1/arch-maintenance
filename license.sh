04aeecb88b05181517de8404734720bc883235e7
pman <pman-$-432894328923@gmail.com>
2024-11-14 14:58:34 +0100

    chore: update file(s): license.sh

--- license.sh
+++ license.sh
@@ -31,7 +31,7 @@ fi
 
 for file in ./src/*rs; do
     if ! grep -q "License" "$file"; then
-        cat "$LICENSE_HEADER" "$file" #> temp && mv temp "$file"
+        cat "$LICENSE_HEADER" "$file" > temp && mv temp "$file"
         echo "Added license to $file"
     else
         echo "License already exists in $file"
