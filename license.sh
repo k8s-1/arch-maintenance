fb76dce8481ca9a6b3dee9ca31c4cac97968a417
pman <pman-$-432894328923@gmail.com>
2024-11-14 14:50:59 +0100

    ...

--- license.sh
+++ license.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-# Path to the license header template
-LICENSE_HEADER="license_header.txt"
-
-# Loop through each .rs file in the src directory
-for file in ./src/**/*rs; do
-    # Check if the file already contains the license header (to avoid duplicates)
+LICENSE_HEADER="./license_header.txt"
+for file in ./src/*rs; do
     if ! grep -q "License" "$file"; then
-        # Add the license header to the top of the file
-        cat "$LICENSE_HEADER" "$file" > temp && mv temp "$file"
+        cat "$LICENSE_HEADER" "$file" #> temp && mv temp "$file"
         echo "Added license to $file"
     else
         echo "License already exists in $file"
